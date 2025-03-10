2628671609cd9085121e2b84ffddf2f6a084ffe5
Mihai <83114335+mihailazar1@users.noreply.github.com>
2025-03-10 21:51:06 +0200

    Update simulator.py

--- Monitoring microservice/simulator.py
+++ Monitoring microservice/simulator.py
@@ -4,7 +4,7 @@ import pika
 import time
 from datetime import datetime
 
-# Load configuration
+
 with open('config.json', 'r') as config_file:
     config = json.load(config_file)
 
@@ -19,7 +19,7 @@ channel = connection.channel()
 
 channel.queue_declare(queue=queue_name, durable=True)
 
-# Open sensor.csv file
+
 with open('sensor.csv', 'r') as csv_file:
     csv_reader = csv.reader(csv_file)
     sensor_values = list(csv_reader)
@@ -28,28 +28,28 @@ print(len(sensor_values))
 
 # Iterate through sensor values and send them to RabbitMQ
 for value_row in sensor_values:
-    measurement_value = float(value_row[0])  # Extract the value
-    timestamp = int(datetime.utcnow().timestamp())  # Current timestamp in s
+    measurement_value = float(value_row[0])  
+    timestamp = int(datetime.utcnow().timestamp())  
 
-    # Create JSON object
+  
     message = {
         "timestamp": timestamp,
         "device_id": device_id,
         "measurement_value": measurement_value
     }
 
-    # Convert to JSON string
+   
     message_json = json.dumps(message)
 
-    # Publish to RabbitMQ
+   
     channel.basic_publish(exchange='',
                           routing_key=queue_name,
                           body=message_json)
 
     print(f"Sent: {message_json}")
 
-    # Wait for 60 seconds before sending the next value
+ 
     time.sleep(1)
 
-# Close the connection
+
 connection.close()
